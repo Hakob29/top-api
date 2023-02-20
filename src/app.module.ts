52da7d6e2514e1376568f89b301d24c7db101f6c
Hakob29 <hakobyan29001@gmail.com>
2023-02-20 22:59:52 +0400

    mongoose was connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
@@ -8,6 +9,7 @@ import { TopPageModule } from './top-page/top-page.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_KEY),
     AuthModule,
     ProductModule,
     ReviewModule,
